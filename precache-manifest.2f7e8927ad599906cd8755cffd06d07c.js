a05b1cfa860b8fd493c37767deed536ec5b8c5fd
Gløer Olav Langslet <gloer@langslet.com>
2019-05-21 23:17:31 +0200

    Updates

---
+++ precache-manifest.2f7e8927ad599906cd8755cffd06d07c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b03d917ae9a10733baacbfdb9d3e9ec",
+    "url": "/eksamen-2018-h/index.html"
+  },
+  {
+    "revision": "517bbe2012b8f9cfc93f",
+    "url": "/eksamen-2018-h/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/eksamen-2018-h/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "517bbe2012b8f9cfc93f",
+    "url": "/eksamen-2018-h/static/js/main.7cade66f.chunk.js"
+  },
+  {
+    "revision": "31b55aa9849afabdb73d",
+    "url": "/eksamen-2018-h/static/js/runtime~main.2849ce62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/eksamen-2018-h/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/eksamen-2018-h/precache-manifest.2f7e8927ad599906cd8755cffd06d07c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eksamen-2018-h/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
